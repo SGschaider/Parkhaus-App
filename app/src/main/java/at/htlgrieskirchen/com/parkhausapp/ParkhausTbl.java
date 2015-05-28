37e031959137a235ea430f84d9e0c8bf82f4278b
sgschaider <Stiwigschaider@gmail.com>
2015-05-28 12:34:17 +0200

    ParkhausTbl erstellt

---
+++ app/src/main/java/at/htlgrieskirchen/com/parkhausapp/ParkhausTbl.java
@@ -0,0 +1,9 @@
+package at.htlgrieskirchen.com.parkhausapp;
+
+/**
+ * Created by sgschaider on 28.05.2015.
+ */
+public class ParkhausTbl
+{
+    public static final String TABLE_NAME = "Parkhaus";
+}
